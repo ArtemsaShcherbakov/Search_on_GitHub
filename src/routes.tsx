380a8a013e968824a92298d9eb0612c38b730393
ArtemsaShcherbakov <sherbrooke2222@gmail.com>
2025-02-16 23:47:46 +0300

    fix: delete base path

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,24 +2,21 @@ import { createHashRouter } from 'react-router-dom';
 import Repositories from './pages/Repositories/index.tsx';
 import Favorites from './pages/Favorites/index.tsx';
 import ProfileRepository from './pages/ProfileRepository/index.tsx';
-import { ROUTES_LIST, BASE_PATH } from './constants.ts';
+import { ROUTES_LIST } from './constants.ts';
 
-const router = createHashRouter(
-  [
-    {
-      path: `/${ROUTES_LIST.repositories}`,
-      element: <Repositories />,
-    },
-    {
-      path: `/${ROUTES_LIST.favorites}`,
-      element: <Favorites />,
-    },
-    {
-      path: `/${ROUTES_LIST.repository}`,
-      element: <ProfileRepository />,
-    },
-  ],
-  { basename: BASE_PATH },
-);
+const router = createHashRouter([
+  {
+    path: `/${ROUTES_LIST.repositories}`,
+    element: <Repositories />,
+  },
+  {
+    path: `/${ROUTES_LIST.favorites}`,
+    element: <Favorites />,
+  },
+  {
+    path: `/${ROUTES_LIST.repository}`,
+    element: <ProfileRepository />,
+  },
+]);
 
 export default router;
